440f44ab4b2d3cae5ef625fd2c7e517dfb5cbbba
chauhanbhupen <122863692+chauhanbhupen@users.noreply.github.com>
2023-07-24 10:32:02 +0530

    S2

--- Week1/S2.py
+++ Week1/S2.py
@@ -1,9 +1,3 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[1]:
-
-
 #Write a python script to enter any number and print the sum of its digit.
 #22bca29 19-06-2023
 
@@ -11,16 +5,3 @@ a=int(input('Enter value of A :'))
 b=int(input('Enter value of B :'))
 c=a+b
 print(c)
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
