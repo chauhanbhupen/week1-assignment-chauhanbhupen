5372a5ae655bb49d30470119dc3261a25c03bad9
chauhanbhupen <122863692+chauhanbhupen@users.noreply.github.com>
2023-07-24 10:27:26 +0530

    SOLUTION-1

---
+++ Week1/S1.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+#write a python script to enter any number to check it is prime or not
+#22bca29   19-06-2023
+num =int(input('enter number: '))
+# If given number is greater than 1
+if num > 1:
+	for i in range(2, int(num/2)+1):
+		if (num % i) == 0:
+			print(num, "is not a prime number")
+			break
+	else:
+		print(num, "is a prime number")
+else:
+	print(num, "is not a prime number")
+
+
+# In[ ]:
+
+
+
+
